9c2e5a9073d1e3b5ec802cf7212ac3d33fdc255c
rafaelbattesti <rafael.battesti@gmail.com>
2016-07-27 20:45:47 -0400

    Initial commit. Added jquery libraries (js, css, themes) and index.html with a basic jquery mobile structure.

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+/*
+ Application : PhoneHub
+ File        : myscript.js
+ Author      : Rafael Battesti
+ Since       : 2016-07-28
+ Version     : 0.1
+*/
+
+//TODO: Add custom scripts for data presentation (AJAX)
