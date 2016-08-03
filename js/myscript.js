82c516049713f66c6615e7766a81b28b4b37a7cf
rafaelbattesti <rafael.battesti@gmail.com>
2016-08-03 15:39:29 -0400

    Added working versions of JQ/JQM in the local library.

--- js/myscript.js
+++
@@ -1,9 +0,0 @@
-/*
- Application : PhoneHub
- File        : myscript.js
- Author      : Rafael Battesti
- Since       : 2016-07-28
- Version     : 0.1
-*/
-
-//TODO: Add custom scripts for data presentation (AJAX)
